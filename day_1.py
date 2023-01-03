e92170fa08519e9cada2611d96d0eb22bab86534
Catarina Esposito Mendes <espositc@uoguelph.ca>
2023-01-03 17:39:17 -0300

    Day 1 - 5th task

--- day_1.py
+++ day_1.py
@@ -1,1 +1,19 @@
-print(len(input("What is your name? ")))
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+x = a
+a = b
+b = x
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
