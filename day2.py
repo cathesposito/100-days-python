04cd7fad824b4a6001b7b12de84d5f22e97fac2b
Catarina Esposito Mendes <espositc@uoguelph.ca>
2023-01-04 13:24:55 -0300

    Day 2 - 1st task

---
+++ day2.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+a = str(two_digit_number)[0]
+b = str(two_digit_number)[1]
+
+x = int(a) + int(b)
+
+print(x)
